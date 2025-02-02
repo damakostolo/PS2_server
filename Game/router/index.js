f6de541dfd5996c81139752b76fc01909950e4f1
damakostolo <teodaniil2@gmail.com>
2025-02-02 21:16:25 +0200

    fix

--- Game/router/index.js
+++ Game/router/index.js
@@ -1,6 +1,6 @@
 const Router = require('express');
 const router = new Router();
-const GameController = require('../controller/GameController');
+const GameController = require('../controller/gameController');
 const checkRole = require('../../middlewares/checkRoleMiddleware');
 
 router.get('/game', GameController.getAll)
